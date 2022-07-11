8e2867ecee3a8cb572f45bdf3b2cd585b9eb36e9
Alyetama <56323389+Alyetama@users.noreply.github.com>
2022-07-11 11:55:41 -0400

    Add support to JSON export

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # twitch-chat-downloader
+
+
+## Installation
+
+```sh
+pip install -r requirements.txt
+```
+
+
+## Usage
+
+```sh
+python twitch-chat.py -c som_channel_name -f 2021/8/31
+```
--- twitch-chat.py
+++ twitch-chat.py
@@ -10,6 +10,7 @@ import signal
 import sys
 import uuid
 from datetime import datetime, timedelta
+from pathlib import Path
 
 import pymongo
 import requests
@@ -24,13 +25,10 @@ def keyboard_interrupt_handler(sig: int, _) -> None:
 
 
 def opts() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(
-        epilog='Export MongoDB connection string as '
-        '`MONGODB_CONNECTION_STRING` before running the script, or add it to '
-        '`.env`.')
+    parser = argparse.ArgumentParser()
     parser.add_argument('-d',
                         '--database-name',
-                        help='MongoDB database name',
+                        help='MongoDB database name (if using MongoDB)',
                         type=str)
     parser.add_argument('-c',
                         '--channel-name',
@@ -41,23 +39,37 @@ def opts() -> argparse.Namespace:
         '--start-from-day',
         help='Start from this date (format: YYYY/M/D, example: 2021/12/31)',
         type=str)
+    parser.add_argument(
+        '-m',
+        '--minimize-json',
+        help='No indentations (smaller file size)',
+        type=str)
     return parser.parse_args()
 
 
-def main(database_name: str, channel_name: str, start_from_day: str) -> None:
+def main(channel_name: str, start_from_day: str, database_name: str = None, minimize_json: bool = False) -> None:
     signal.signal(signal.SIGINT, keyboard_interrupt_handler)
 
-    client = pymongo.MongoClient(os.environ['MONGODB_CONNECTION_STRING'])
-    db = client[database_name]
-    col = db[channel_name]
-    metadata_col = db[f'{channel_name}_metadata']
+    USE_MONGODB = False
+    if os.getenv('MONGODB_CONNECTION_STRING'):
+        USE_MONGODB = True
+        if not database_name:
+            raise AssertionError('Database name is required!')
+
+    if USE_MONGODB:
+        client = pymongo.MongoClient(os.environ['MONGODB_CONNECTION_STRING'])
+        db = client[database_name]
+        col = db[channel_name]
+        metadata_col = db[f'{channel_name}_metadata']
+        existing_dates = [
+            str(x['date'].date()).replace('-', '/')
+            for x in list(metadata_col.find({}))
+        ]
+    else:
+        Path(channel_name).mkdir(exist_ok=True)
+        existing_dates = []
 
     first_day_obj = datetime.strptime(start_from_day, '%Y/%m/%d').date()
-
-    existing_dates = [
-        str(x['date'].date()).replace('-', '/')
-        for x in list(metadata_col.find({}))
-    ]
 
     i = 1
     dates = []
@@ -78,23 +90,32 @@ def main(database_name: str, channel_name: str, start_from_day: str) -> None:
         resp = requests.get(
             f'https://logs.ivr.fi/channel/{channel_name}/{date_path}?json=true'
         )
+
         data = resp.json()
         _data = copy.deepcopy(data)
+        data_dict = {}
 
         for msg in data['messages']:
             msg.update({'_id': str(uuid.uuid4())})
 
-        col.insert_many(data['messages'])
+        if USE_MONGODB:
+            col.insert_many(data['messages'])
+        else:
+            with open(f'{channel_name}/{date}.json', 'w') as j:
+                if minimize_json:
+                    json.dump(data, j)
+                else:
+                    json.dump(data, j, indent=4)
 
-        md5_checksum = hashlib.md5(
-            json.dumps(_data).encode('utf-8')).hexdigest()
-
-        date_obj = datetime.strptime(date_path, '%Y/%m/%d')
-        metadata_col.insert_one({
-            '_id': md5_checksum,
-            'date': date_obj,
-            'messages': len(data['messages'])
-        })
+        if USE_MONGODB:
+            md5_checksum = hashlib.md5(
+                json.dumps(_data).encode('utf-8')).hexdigest()
+            date_obj = datetime.strptime(date_path, '%Y/%m/%d')
+            metadata_col.insert_one({
+                '_id': md5_checksum,
+                'date': date_obj,
+                'messages': len(data['messages'])
+            })
 
 
 if __name__ == '__main__':
